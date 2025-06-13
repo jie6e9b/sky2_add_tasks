1c4fbc6d884aad2ecc633fd901b246587265f886
Владимир Лебедев <lvv@MacBook-Pro-Vladimir.local>
2025-06-13 20:56:53 +0700

    add new func middle_price and conditions about zero quantity. Plus add tests

--- lesson6/employee.py
+++ lesson6/employee.py
@@ -15,15 +15,36 @@
 
 
 class Employee:
-    pass
+    def __init__(self, name, age, salary):
+        if age < 18 or age > 127:
+            raise ValueError('возраст должен быть в пределах от 18 до 127 лет')
+        if salary < 16242:
+            raise  ValueError('зараплата должна быть больше прожиточного минимума 16242р')
+
+        self.name = name
+        self.age = age
+        self.salary = salary
+    def __str__(self):
+        return f"{self.name}, возраст: {self.age}, зпшка: {self.salary}"
 
 
-# код для проверки
-employee = Employee('John', 30, 5000)
-# raises ValueError('Оплата труда не может быть меньше 16242')
+if __name__ == '__main__':
 
-employee = Employee("Jane", 17, 50000)
-# raises ValueError('Возраст должен быть не меньше 18 и не больше 127')
+    # код для проверки
+    try:
+        employee = Employee('John', 30, 5000)
+    except ValueError as e:
+        print(e)
+        # raises ValueError('Оплата труда не может быть меньше 16242')
 
-employee = Employee("Kate", 175, 50000)
-# raises ValueError('Возраст должен быть не меньше 18 и не больше 127')
+    try:
+        employee = Employee("Jane", 17, 50000)
+    except ValueError as e:
+        print(e)
+        # raises ValueError('Возраст должен быть не меньше 18 и не больше 127')
+
+    try:
+        employee = Employee("Kate", 175, 50000)
+        # raises ValueError('Возраст должен быть не меньше 18 и не больше 127')
+    except ValueError as e:
+        print(e)
